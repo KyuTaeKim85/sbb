9a5f8276ab6eea9fe0734b508182da0f3d1ac7dd
KyuTaeKim85 <k31726@nate.com>
2025-01-12 17:16:26 +0900

    테스트1

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
     @GetMapping("/")
     public String root(){
-        //System.out.println("메인 리스트로 이동");
+        System.out.println("메인 리스트로 이동");
         return "redirect:/question/list";
     }
 }
