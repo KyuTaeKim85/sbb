c9aec076ba7712b41a7e1192f9a55abe344371c5
KyuTaeKim85 <k31726@nate.com>
2025-01-12 09:37:39 +0900

    test

--- src/main/java/com/mysite/sbb/answer/Answer.java
+++ src/main/java/com/mysite/sbb/answer/Answer.java
@@ -1,12 +1,16 @@
 package com.mysite.sbb.answer;
 
 
+import com.mysite.sbb.comment.Comment;
 import com.mysite.sbb.question.Question;
+import com.mysite.sbb.user.SiteUser;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -19,8 +23,28 @@ public class Answer {
     @Column(columnDefinition = "TEXT")  //문자열 길이 제한 제거
     private String content;
 
-    private LocalDateTime createDate;
+    private LocalDateTime createDate;   //답변 데이터 생성일자
+    private LocalDateTime modifyDate;   //답변 데이터 수정일자
 
     @ManyToOne  //Answer(답변)여러개이고, Question(질문)은 1개 -> N:1
     private Question question;  //Question 클래스
+
+    @ManyToOne
+    private SiteUser author;
+
+    @ManyToMany
+    Set<SiteUser> voter;
+
+    @OneToMany(mappedBy = "answer")
+    private List<Comment> commentList;
+
+    @Override
+    public String toString() {
+        return "Answer{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", createDate=" + createDate +
+                ", modifyDate=" + modifyDate +
+                '}';
+    }
 }
